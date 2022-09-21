3e1f23840dc26f5f140911a770e0cc7870a89cbc
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-21 14:07:56 -0500

    feat: add searchCapital method on country service

--- src/app/countries/pages/by-capital/by-capital.component.ts
+++ src/app/countries/pages/by-capital/by-capital.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital',
   templateUrl: './by-capital.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class ByCapitalComponent implements OnInit {
+export class ByCapitalComponent {
+  searchTerm: string = '';
+  hasError: boolean = false;
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
+  search(searchTerm: string) {
+    this.hasError = false;
+    this.searchTerm = searchTerm;
+
+    this.countryService.searchCapital(this.searchTerm).subscribe(
+      (countries) => {
+        this.countries = countries;
+      },
+      (err) => {
+        this.hasError = true;
+        this.countries = [];
+      }
+    );
   }
-
 }
