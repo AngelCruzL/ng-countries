ed81bcdb09cafc02c93dce0c280a1c148b1f8ead
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-22 08:50:40 -0500

    feat: add search by region layout

--- src/app/countries/pages/by-region/by-region.component.ts
+++ src/app/countries/pages/by-region/by-region.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-inline-start: 5px;
+      }
+    `,
+  ],
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  activeRegion: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  getCssClass(region: string): string {
+    return region === this.activeRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activateRegion(region: string) {
+    this.activeRegion = region;
+
+    // TODO: Service call
+  }
 }
